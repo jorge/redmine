0711de5a3bb934ce168513e16435743ffd2af9f2
Marius Hanne <marius.hanne@sourceagency.org>
2011-09-07 11:40:35 +0200

    Rails3: TODO: comment out format.api calls in controllers for now
    --HG--
    branch : rails3-straight-20120331-1
    extra : transplant_source : -id%D1%94R%1EJ%AFm%3C%21%AD%E9%03T%93%2B%9DC

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -77,7 +77,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       format.html { render :layout => 'base' }
-      format.api
+#      format.api
     end
   end
 
@@ -109,7 +109,7 @@ class UsersController < ApplicationController
             {:controller => 'users', :action => 'edit', :id => @user}
           )
         }
-        format.api  { render :action => 'show', :status => :created, :location => user_url(@user) }
+#        format.api  { render :action => 'show', :status => :created, :location => user_url(@user) }
       end
     else
       @auth_sources = AuthSource.find(:all)
@@ -118,7 +118,7 @@ class UsersController < ApplicationController
 
       respond_to do |format|
         format.html { render :action => 'new' }
-        format.api  { render_validation_errors(@user) }
+#        format.api  { render_validation_errors(@user) }
       end
     end
   end
@@ -156,7 +156,7 @@ class UsersController < ApplicationController
           flash[:notice] = l(:notice_successful_update)
           redirect_to :back
         }
-        format.api  { head :ok }
+#        format.api  { head :ok }
       end
     else
       @auth_sources = AuthSource.find(:all)
@@ -166,7 +166,7 @@ class UsersController < ApplicationController
 
       respond_to do |format|
         format.html { render :action => :edit }
-        format.api  { render_validation_errors(@user) }
+#        format.api  { render_validation_errors(@user) }
       end
     end
   rescue ::ActionController::RedirectBackError

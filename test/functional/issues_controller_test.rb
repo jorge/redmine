c9258ec9d2c9729f0c9695904070c37e8c2bf319
Sylvain Utard - redox <sylvain.utard@gmail.com>
2011-10-03 17:10:28 +0900

    Rails3: test: mail: mail's bodies must be checked calling 'encoded'
    --HG--
    branch : rails3-straight-20120213-7
    extra : transplant_source : %D6%CD%3E%CEM%07m%7F%A2%E8k%8E%165%C0s%23T0%8E

--- test/functional/issues_controller_test.rb
+++ test/functional/issues_controller_test.rb
@@ -2105,7 +2105,7 @@ class IssuesControllerTest < ActionController::TestCase
     mail = ActionMailer::Base.deliveries.last
     assert_kind_of Mail::Message, mail
     assert mail.subject.starts_with?("[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}]")
-    assert mail.body.include?("Subject changed from #{old_subject} to #{new_subject}")
+    assert mail.body.encoded.include?("Subject changed from #{old_subject} to #{new_subject}")
   end
 
   def test_put_update_with_project_change
@@ -2179,7 +2179,7 @@ class IssuesControllerTest < ActionController::TestCase
 
     mail = ActionMailer::Base.deliveries.last
     assert_kind_of Mail::Message, mail
-    assert mail.body.include?("Searchable field changed from 125 to New custom value")
+    assert mail.body.encoded.include?("Searchable field changed from 125 to New custom value")
   end
 
   def test_put_update_with_multi_custom_field_change
@@ -2222,7 +2222,7 @@ class IssuesControllerTest < ActionController::TestCase
     assert_equal 2, j.details.size
 
     mail = ActionMailer::Base.deliveries.last
-    assert mail.body.include?("Status changed from New to Assigned")
+    assert mail.body.encoded.include?("Status changed from New to Assigned")
     # subject should contain the new status
     assert mail.subject.include?("(#{ IssueStatus.find(2).name })")
   end
@@ -2240,7 +2240,7 @@ class IssuesControllerTest < ActionController::TestCase
     assert_equal User.anonymous, j.user
 
     mail = ActionMailer::Base.deliveries.last
-    assert mail.body.include?(notes)
+    assert mail.body.encoded.include?(notes)
   end
 
   def test_put_update_with_note_and_spent_time
@@ -2298,7 +2298,7 @@ class IssuesControllerTest < ActionController::TestCase
     assert_equal 59, File.size(attachment.diskfile)
 
     mail = ActionMailer::Base.deliveries.last
-    assert mail.body.include?('testfile.txt')
+    assert mail.body.encoded.include?('testfile.txt')
   end
 
   def test_put_update_with_attachment_that_fails_to_save

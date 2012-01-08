a67d14ebffef89e0c0837b502fa5b136fed41bf2
Toshi MARUYAMA <marutosijp2@gmail.com>
2012-01-08 20:01:22 +0900

    Rails3: new route: custom_fields
    --HG--
    branch : rails3-straight-20120331-1
    extra : transplant_source : %00%B1X%89ntd%FD%1A%EC34_%10%C8%E2%8FCQ%E1

--- config/routes.rb
+++ config/routes.rb
@@ -337,7 +337,7 @@ Redmine::Application.routes.draw do |map|
       post 'update_issue_done_ratio'
     end
   end
-  map.resources :custom_fields, :except => :show
+  resources :custom_fields, :except => :show
   map.resources :roles, :except => :show, :collection => {:permissions => [:get, :post]}
   map.resources :enumerations, :except => :show
 
